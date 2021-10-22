02ffc813a13b3ef8776c6170701ea7321da60bab
Rafaela Silva <rafa_ss04@hotmail.com>
2021-10-21 22:44:37 -0300

    Criação DataAccess para acesso de banco de dados.

--- FacialBiometricsBack/Startup.cs
+++ FacialBiometricsBack/Startup.cs
@@ -1,3 +1,4 @@
+using FacialBiometricsBack.DataAccessFacialBiometrics;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -18,6 +19,7 @@ namespace FacialBiometrics
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IDataAccessFacialBiometrics, DataAccessFacialBiometrics>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
