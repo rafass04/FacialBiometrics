1095651ab88ef27e50441f0ded2e60ace20cba6b
Alexandre Santos <ale.santos49@gmail.com>
2021-11-09 22:38:24 -0300

    Novo diretório de upload das imagens

--- FacialBiometricsBack/Services/EmguService.cs
+++ FacialBiometricsBack/Services/EmguService.cs
@@ -19,18 +19,30 @@ namespace FacialBiometricsBack.Services
 
         public bool CompareImages(List<UsersFacialBiometrics> imgsDb, List<byte[]> imgsRecebidas)
         {
-            Image<Gray, Byte>[] imgsTraining = new Image<Gray, byte>[4];
+            Image<Gray, byte>[] imgsTraining = new Image<Gray, byte>[4];
+            VectorOfMat imageList = new VectorOfMat();
+            VectorOfInt labelList = new VectorOfInt();
+
+
+            string pathUpload = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"Uploads\\");
+            DirectoryInfo dir = Directory.CreateDirectory(pathUpload);
 
             List<string> pathImgs = new List<string>();
+            string pathImgReceived;
+
+            pathImgReceived = pathUpload + "imgReceived" + ".jpeg";
+
+            using (var fs = new FileStream(pathImgReceived, FileMode.Create, FileAccess.Write))
+            {
+                fs.Write(imgsRecebidas[0], 0, imgsRecebidas[0].Length);
+            }
+
             try
             {
-                string caminho = @"./Uploads/";
-                DirectoryInfo dir = Directory.CreateDirectory(caminho);
-
 
                 foreach (var img in imgsDb)
                 {
-                    string path = caminho + img.IdUser + img.IdImg + ".jpeg";
+                    string path = pathUpload + img.IdUser + img.IdImg + ".jpeg";
                     pathImgs.Add(path);
 
                     using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write))
@@ -39,27 +51,30 @@ namespace FacialBiometricsBack.Services
                     }
 
                 }
-                
+
+                for (int i = 0; i < imgsTraining.Length; i++)
+                {
+                    imgsTraining[i] = new Image<Gray, byte>(pathImgs[i]);
+                    imageList.Push(imgsTraining[i].Mat);
+                    labelList.Push(new[] { i });
+                }
+
+                Image<Gray, Byte> imagemRecebida = new Image<Gray, Byte>(pathImgReceived);
+
+                EigenFaceRecognizer recognizer = new EigenFaceRecognizer(imageList.Size);//Altura 1280 e Largura 958
+
+                recognizer.Train(imageList, labelList);
+                EigenFaceRecognizer.PredictionResult result = recognizer.Predict(imagemRecebida.Resize(958,1280,Inter.Cubic));
+                 
+                var respostaEmgu = result.Label;
+
             }
             catch (Exception e)
             {
                 Console.WriteLine("Erro: " + e.Message);
+                throw new Exception("Erro CompareImages: "+e.Message);
             }
 
-            for (int i = 0; i < imgsTraining.Length; i++)
-            {
-                imgsTraining[i] = new Image<Gray, Byte>(pathImgs[i]);
-            }
-
-            int[] labels = new int[] { 0, 1, 2 };
-
-            Image<Gray, Byte> imagemRecebida = new Image<Gray, Byte>(Encoding.ASCII.GetString(imgsRecebidas[0]));
-
-            EigenFaceRecognizer recognizer = new EigenFaceRecognizer(0, 0.2);//definir o tamanho (largura e altura) da imagem recebida
-            EigenFaceRecognizer.PredictionResult result = recognizer.Predict(imagemRecebida);
-
-            var respostaEmgu = result.Label;
-            
             //retorno temporário
             return true;
         }
